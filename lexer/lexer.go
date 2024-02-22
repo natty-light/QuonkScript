a80a53b2dacdfa8d48e27ee640b4ab816d4bd21f
Natasha <75773163+natty-light@users.noreply.github.com>
2024-02-21 21:44:45 -0500

    Add logical comparison tokens to tokenizer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -32,6 +32,12 @@ const (
 	OpenSquareBracket
 	CloseSquareBracket
 	Dot
+	Equality
+	GreaterThan
+	LessThan
+	GreaterEqualTo
+	LessEqualTo
+	NotEqual
 
 	EOF // End of File
 )
@@ -53,6 +59,12 @@ const (
 	leftSquareBracket  = "["
 	rightSquareBracket = "]"
 	dot                = "."
+	greaterThan        = ">"
+	lessThan           = "<"
+	equality           = "=="
+	leq                = "<="
+	geq                = ">="
+	bang               = "!"
 )
 
 type Token struct {
@@ -89,6 +101,7 @@ func Tokenize(source string) []Token {
 	keywords := getKeywordMap()
 
 	src := strings.Split(source, "")
+	remaining := len(src)
 
 	// Build each token
 	for len(src) > 0 {
@@ -101,21 +114,31 @@ func Tokenize(source string) []Token {
 			tokens = append(tokens, token(OpenParen, char))
 			// Pop first char
 			src = utils.Pop(src)
+			remaining--
+
 		case rightParen:
 			tokens = append(tokens, token(CloseParen, char))
 			src = utils.Pop(src)
+			remaining--
+
 		case leftCurlyBracket:
 			tokens = append(tokens, token(OpenCurlyBracket, char))
 			src = utils.Pop(src)
+			remaining--
+
 		case rightCurlyBracket:
 			tokens = append(tokens, token(CloseCurlyBracket, char))
 			src = utils.Pop(src)
+			remaining--
+
 		case leftSquareBracket:
 			tokens = append(tokens, token(OpenSquareBracket, char))
 			src = utils.Pop(src)
+			remaining--
 		case rightSquareBracket:
 			tokens = append(tokens, token(CloseSquareBracket, char))
 			src = utils.Pop(src)
+			remaining--
 		case addSym:
 			fallthrough
 		case subSym:
@@ -127,21 +150,88 @@ func Tokenize(source string) []Token {
 		case multSym:
 			tokens = append(tokens, token(BinaryOperator, char))
 			src = utils.Pop(src)
+			remaining--
 		case eqSym:
-			tokens = append(tokens, token(Equals, char))
+			// check for equality symbol here
 			src = utils.Pop(src)
+			remaining--
+			if remaining > 0 { // If there are symbols left to parse
+				nextChar := src[0] // This is safe because remaining > 0
+				// another equals sign
+				if nextChar == eqSym {
+					tokens = append(tokens, token(Equality, "=="))
+					src = utils.Pop(src)
+					remaining--
+				} else {
+					tokens = append(tokens, token(Equals, char))
+				}
+			} else {
+				tokens = append(tokens, token(Equals, char))
+			}
+		case greaterThan: // >= or >
+			// need to check next char
+			src = utils.Pop(src)
+			remaining--
+			if remaining > 0 { // If there are symbols left to parse
+				nextChar := src[0] // This is safe because remaining > 0
+				// looking for equals sign
+				if nextChar == eqSym {
+					tokens = append(tokens, token(GreaterEqualTo, ">="))
+					src = utils.Pop(src)
+					remaining--
+				} else {
+					tokens = append(tokens, token(GreaterThan, char))
+				}
+			} else {
+				tokens = append(tokens, token(GreaterThan, char))
+			}
+		case lessThan: // <= or <
+			// need to check next char
+			src = utils.Pop(src)
+			remaining--
+			if remaining > 0 { // If there are symbols left to parse
+				nextChar := src[0] // This is safe because remaining > 0
+				// looking for equals sign
+				if nextChar == eqSym {
+					tokens = append(tokens, token(LessEqualTo, "<="))
+					src = utils.Pop(src)
+					remaining--
+				} else {
+					tokens = append(tokens, token(LessThan, char))
+				}
+			} else {
+				tokens = append(tokens, token(LessThan, char))
+			}
+		case bang:
+			// need to check next char
+			src = utils.Pop(src)
+			remaining--
+			if remaining > 0 { // If there are symbols left to parse
+				nextChar := src[0] // This is safe because remaining > 0
+				// looking for equals sign
+				if nextChar == eqSym {
+					tokens = append(tokens, token(NotEqual, "!="))
+					src = utils.Pop(src)
+					remaining--
+				}
+			}
+
 		case semi:
 			tokens = append(tokens, token(Semicolon, char))
 			src = utils.Pop(src)
+			remaining--
 		case colon:
 			tokens = append(tokens, token(Colon, char))
 			src = utils.Pop(src)
+			remaining--
 		case comma:
 			tokens = append(tokens, token(Comma, char))
 			src = utils.Pop(src)
+			remaining--
 		case dot:
 			tokens = append(tokens, token(Dot, char))
 			src = utils.Pop(src)
+			remaining--
 		default:
 			// Handle multichar token
 			if isNumeric(char) {
@@ -151,6 +241,7 @@ func Tokenize(source string) []Token {
 				for len(src) > 0 && isNumeric(src[0]) {
 					num += src[0]
 					src = utils.Pop(src)
+					remaining--
 				}
 
 				tokens = append(tokens, token(Number, num))
@@ -160,6 +251,7 @@ func Tokenize(source string) []Token {
 				for len(src) > 0 && isAlpha(src[0]) {
 					ident += src[0]
 					src = utils.Pop(src)
+					remaining--
 				}
 
 				// check for reserved keyword
@@ -175,6 +267,7 @@ func Tokenize(source string) []Token {
 
 			} else if isSkipable(char) {
 				src = utils.Pop(src)
+				remaining--
 			} else {
 				panic(fmt.Sprintf("Unrecognized character %s", char))
 			}
